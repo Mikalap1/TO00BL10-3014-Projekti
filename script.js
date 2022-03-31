4c512a665d8aad640ceef0b61c8dbee9bc780adf
Mikalap1 <mika.lappalainen@student.laurea.fi>
2022-03-31 17:18:42 +0300

    3103

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Javascript
 
+
+
+
 // Sulje nappi
 
 var myNodelist = document.getElementsByTagName("li");
@@ -23,13 +26,13 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Listaan lisääminen napista
-function newElement() {
+function newTask() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("input").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
-      alert("You must write something!");
+      alert("Kirjoita jotain!");
     } else {
       document.getElementById("lista").appendChild(li);
     }
@@ -47,6 +50,14 @@ function newElement() {
         div.style.display = "none";
       }
     }
+
+    // Lisätään TODO local storageen
+    saveLocalTodos(inputValue);
+
+
+
+
+
 // Check merkki klikkauksella
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
@@ -54,5 +65,38 @@ list.addEventListener('click', function(ev) {
     ev.target.classList.toggle('tehty');
   }
 }, false);
+}
+// Local storage
+function saveLocalTodos(newTask){
+  // TARKISTUS: Onko listalla jo asioita
+  let todos;
+  if(localStorage.getItem('todos') === null){
+    todos = [];
+  }
+  else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  // Jos ei ole tallessa, tee uusi
+  todos.push(newTask);
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+
+function GetTodos() {
+  let todos;
+  if(localStorage.getItem('todos') === null){
+    todos = [];
+  }
+  else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+}}
+
+
+// Listan päivittäminen LocalStoragesta
+localStorage.getItem(todos);
+document.createElement("li");
+
 
 }
+
+
